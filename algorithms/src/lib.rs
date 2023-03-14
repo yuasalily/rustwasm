1eee571efef8cbb54c89433fc5ea0013e56e5b34
yuasalily <yuasalily1011@gmail.com>
2023-03-15 00:18:50 +0900

    素数判定を追加

--- algorithms/src/lib.rs
+++ algorithms/src/lib.rs
@@ -1,8 +1,18 @@
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub fn is_prime(n: u64) -> bool {
+    if n < 2 {
+        return false;
+    }
+    let mut i: u64 = 2;
+    while i * i <= n {
+        if n % i == 0 {
+            return false;
+        }
+        i += 1;
+    }
+    true
 }
 
 #[cfg(test)]
@@ -10,8 +20,13 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn is_prime_test() {
+        assert_eq!(is_prime(1), false);
+        assert_eq!(is_prime(2), true);
+        assert_eq!(is_prime(3), true);
+        assert_eq!(is_prime(57), false);
+        assert_eq!(is_prime(30001), false);
+        assert_eq!(is_prime(8635844967113809), false);
+        assert_eq!(is_prime(9007199254740881), true);
     }
 }
