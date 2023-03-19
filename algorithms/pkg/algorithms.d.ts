b1705a2729c14d2f974d3b28d03bd33fd47970b9
yuasalily <yuasalily1011@gmail.com>
2023-03-19 18:36:32 +0900

    wasm更新

--- algorithms/pkg/algorithms.d.ts
+++ algorithms/pkg/algorithms.d.ts
@@ -1,16 +1,29 @@
 /* tslint:disable */
 /* eslint-disable */
 /**
+* 素数判定をする関数です。
+* 返り値が0なら素数、0以外なら少なくともその値で割り切れる。
+* * `n` - 1以上の18446744073709551615以下の自然数
 * @param {bigint} n
-* @returns {boolean}
+* @returns {bigint}
 */
-export function is_prime(n: bigint): boolean;
+export function is_prime(n: bigint): bigint;
+/**
+* 素因数分解をする関数です。
+* * `n` - 1以上の18446744073709551615以下の自然数
+* @param {bigint} n
+* @returns {string}
+*/
+export function prime_factorization(n: bigint): string;
 
 export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
 
 export interface InitOutput {
   readonly memory: WebAssembly.Memory;
   readonly is_prime: (a: number) => number;
+  readonly prime_factorization: (a: number, b: number) => void;
+  readonly __wbindgen_add_to_stack_pointer: (a: number) => number;
+  readonly __wbindgen_free: (a: number, b: number) => void;
 }
 
 export type SyncInitInput = BufferSource | WebAssembly.Module;
--- algorithms/pkg/algorithms_bg.wasm.d.ts
+++ algorithms/pkg/algorithms_bg.wasm.d.ts
@@ -2,3 +2,6 @@
 /* eslint-disable */
 export const memory: WebAssembly.Memory;
 export function is_prime(a: number): number;
+export function prime_factorization(a: number, b: number): void;
+export function __wbindgen_add_to_stack_pointer(a: number): number;
+export function __wbindgen_free(a: number, b: number): void;
