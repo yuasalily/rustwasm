0700c2490c4825e38bc1d8735aabd5dea35f00ba
yuasalily <yuasalily1011@gmail.com>
2023-04-08 18:51:25 +0900

    webpack用に更新

--- algorithms/pkg/algorithms.d.ts
+++ algorithms/pkg/algorithms.d.ts
@@ -15,34 +15,3 @@ export function is_prime(n: bigint): bigint;
 * @returns {string}
 */
 export function prime_factorization(n: bigint): string;
-
-export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
-
-export interface InitOutput {
-  readonly memory: WebAssembly.Memory;
-  readonly is_prime: (a: number) => number;
-  readonly prime_factorization: (a: number, b: number) => void;
-  readonly __wbindgen_add_to_stack_pointer: (a: number) => number;
-  readonly __wbindgen_free: (a: number, b: number) => void;
-}
-
-export type SyncInitInput = BufferSource | WebAssembly.Module;
-/**
-* Instantiates the given `module`, which can either be bytes or
-* a precompiled `WebAssembly.Module`.
-*
-* @param {SyncInitInput} module
-*
-* @returns {InitOutput}
-*/
-export function initSync(module: SyncInitInput): InitOutput;
-
-/**
-* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
-* for everything else, calls `WebAssembly.instantiate` directly.
-*
-* @param {InitInput | Promise<InitInput>} module_or_path
-*
-* @returns {Promise<InitOutput>}
-*/
-export default function init (module_or_path?: InitInput | Promise<InitInput>): Promise<InitOutput>;
